8208a311bfd85a1f4aa1b8be2512f457ab776785
kaimannweiler <kai.mannweiler@hotmail.de>
2024-12-01 22:13:07 +0100

    Day1

---
+++ adventofcode_1.js
@@ -0,0 +1,58 @@
+/* const fs = require('node:fs');
+fs.readFile('/Users/kaima/input.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const leftArray = [];
+  const rightArray = [];
+  const inputArray = data.split(/ +|\n/)
+  inputArray.forEach((input, index) => {
+    if (input == "") return;
+    if (index % 2 === 0) 
+      leftArray.push(input)
+    else
+      rightArray.push(input)
+  })
+  
+  leftArray.sort()
+  rightArray.sort()
+
+  let sum = leftArray.reduce((previous, current, index, array) =>{
+    let difference = Math.abs(current - rightArray[index])
+    console.log(current, rightArray[index], difference, previous)
+    return previous + difference
+  }, 0)
+  console.log(sum)
+});
+ */
+  
+
+const fs = require('node:fs');
+fs.readFile('/Users/kaima/input2.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const leftArray = [];
+  const rightArray = [];
+  const inputArray = data.split(/ +|\n/)
+  inputArray.forEach((input, index) => {
+    if (input == "") return;
+    if (index % 2 === 0) 
+      leftArray.push(input)
+    else
+      rightArray.push(input)
+  })
+  
+
+  let sum = leftArray.reduce((previous, current, index, array) =>{
+    let doubles = rightArray.filter((value) => value === current)
+    let score = doubles.length * current
+    console.log(current, doubles, score, previous)
+    return previous + score
+  }, 0)
+  console.log(sum)
+
+  
+});
